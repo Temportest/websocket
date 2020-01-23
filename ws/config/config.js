a4d16d3a74678904eb983c6f261d0efbfd5e2af9
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-01-23 17:11:35 +0800

    modify config

--- ws/config/config.js
+++ ws/config/config.js
@@ -4,29 +4,35 @@ require('dotenv').config();
 
 const envVarSchema = Joi.object().keys({
     NODE_ENV: Joi.string().default('development').allow(['development', 'production']), // 字串且預設值為development 並只允許兩種參數
-    WS_HOST: Joi.string().when(
-      'NODE_ENV',
-      {
-        is: Joi.string().equal('production'),
-        then: Joi.string().default(process.env['PDU_HOST']),
-        otherwise: Joi.string().default(process.env['DEV_HOST'])
-      },
+    WS_HOST: Joi.string()
+    .when(
+      'NODE_ENV', { is: 'development', then: Joi.string().default(process.env['PDU_HOST']) }
+      )
+    .when(
+      'NODE_ENV', { is: 'WEBDUINO', then: Joi.string().default(process.env['WEBDUINO_HOST']) }
+    )
+    .when(
+      'NODE_ENV', { is: 'NUWA', then: Joi.string().default(process.env['NUWA_HOST']) }
     ),
-    WS_USERNAME: Joi.string().when(
-        'NODE_ENV',
-        {
-          is: Joi.string().equal('production'),
-          then: Joi.string().default(process.env['PDU_USERNAME']),
-          otherwise: Joi.string().default(process.env['DEV_USERNAME'])
-        },
-      ),
-      WS_PASSWORD: Joi.string().when(
-        'NODE_ENV',
-        {
-          is: Joi.string().equal('production'),
-          then: Joi.string().default(process.env['PDU_PASSWORD']),
-          otherwise: Joi.string().default(process.env['DEV_PASSWORD'])
-        },
+    WS_USERNAME: Joi.string()
+    .when(
+      'NODE_ENV', { is: 'development', then: Joi.string().default(process.env['PDU_USERNAME']) }
+      )
+    .when(
+      'NODE_ENV', { is: 'WEBDUINO', then: Joi.string().default(process.env['WEBDUINO_USERNAME']) }
+    )
+    .when(
+      'NODE_ENV', { is: 'NUWA', then: Joi.string().default(process.env['NUWA_USERNAME']) }
+    ),
+      WS_PASSWORD: Joi.string()
+      .when(
+        'NODE_ENV', { is: 'development', then: Joi.string().default(process.env['PDU_PASSWORD']) }
+        )
+      .when(
+        'NODE_ENV', { is: 'WEBDUINO', then: Joi.string().default(process.env['WEBDUINO_PASSWORD']) }
+      )
+      .when(
+        'NODE_ENV', { is: 'NUWA', then: Joi.string().default(process.env['NUWA_PASSWORD']) }
       )
   }).unknown().required();
 
@@ -38,7 +44,7 @@ if (error) {
 }
 
 const config={
-    env: envVars.NODE_ENV, // 開發模式(development、production)
+    env: envVars.NODE_ENV, // 開發模式(development、WEBDUINO、NUWA)
     wsHost: envVars.WS_HOST,
     wsUsername:envVars.WS_USERNAME,
     wsPassword:envVars.WS_PASSWORD
